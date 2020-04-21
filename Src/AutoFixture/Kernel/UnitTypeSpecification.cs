d3ad9b979adc4574b537299e3e4f747dad9001c4
Enrico Campidoglio <enrico.campidoglio@gmail.com>
2020-04-21 14:13:55 +0200

    Adds a specification to recognize unit types

---
+++ Src/AutoFixture/Kernel/UnitTypeSpecification.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace AutoFixture.Kernel
+{
+    public class UnitTypeSpecification : IRequestSpecification
+    {
+        public bool IsSatisfiedBy(object request)
+        {
+            if (request == null)
+            {
+                throw new ArgumentNullException(nameof(request));
+            }
+
+            return request is Type type
+                   && type.IsValueType
+                   && HasNoPublicProperties(type)
+                   && HasNoPublicFields(type);
+        }
+
+        private static bool HasNoPublicProperties(Type type) =>
+            !type.GetProperties().Any();
+
+        private static bool HasNoPublicFields(Type type) =>
+            !type.GetFields().Any();
+    }
+}
---
+++ Src/AutoFixtureUnitTest/Kernel/UnitTypeSpecificationTest.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using AutoFixture.Kernel;
+using TestTypeFoundation;
+using Xunit;
+
+namespace AutoFixtureUnitTest.Kernel
+{
+    public class UnitTypeSpecificationTest
+    {
+        [Fact]
+        public void SutIsRequestSpecification()
+        {
+            // Arrange
+            // Act
+            var sut = new UnitTypeSpecification();
+
+            // Assert
+            Assert.IsAssignableFrom<IRequestSpecification>(sut);
+        }
+
+        [Fact]
+        public void IsSatisfiedByWithNullShouldThrowArgumentNullException()
+        {
+            // Arrange
+            var sut = new UnitTypeSpecification();
+
+            // Act & assert
+            Assert.Throws<ArgumentNullException>(() => sut.IsSatisfiedBy(null));
+        }
+
+        [Theory]
+        [InlineData("string")]
+        [InlineData(4)]
+        [InlineData(4.9D)]
+        [InlineData(true)]
+        public void IsSatisfiedByWithSomethingOtherThanTypeShouldReturnFalse(object request)
+        {
+            // Arrange
+            var sut = new UnitTypeSpecification();
+
+            // Act
+            var result = sut.IsSatisfiedBy(request);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Theory]
+        [InlineData(typeof(MutableValueType))]
+        [InlineData(typeof(MutableValueTypeWithoutConstructor))]
+        [InlineData(typeof(MutableValueTypeWithField))]
+        [InlineData(typeof(MutableValueTypeWithFieldWithoutConstructor))]
+        public void IsSatisfiedByWithMutableValueTypeWithMembersShouldReturnFalse(object request)
+        {
+            // Arrange
+            var sut = new UnitTypeSpecification();
+
+            // Act
+            var result = sut.IsSatisfiedBy(request);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Theory]
+        [InlineData(typeof(ImmutableValueTypeWithProperty))]
+        [InlineData(typeof(ImmutableValueTypeWithField))]
+        public void IsSatisfiedByWithImmutableValueTypeWithMembersShouldReturnFalse(object request)
+        {
+            // Arrange
+            var sut = new UnitTypeSpecification();
+
+            // Act
+            var result = sut.IsSatisfiedBy(request);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void IsSatisfiedByWithUnitTypeShouldReturnTrue()
+        {
+            // Arrange
+            var sut = new UnitTypeSpecification();
+
+            // Act
+            var result = sut.IsSatisfiedBy(typeof(UnitType));
+
+            // Assert
+            Assert.True(result);
+        }
+    }
+}
